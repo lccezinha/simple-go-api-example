16b4ce0444126667b9d8e8e983c0a48493ef17d5
Luiz Cezer <lccezinha@gmail.com>
2015-12-25 20:36:41 -0200

    Add mais rotas e utilizando parametros da url

--- app.go
+++ app.go
@@ -12,9 +12,22 @@ import (
 func main() {
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/", Index)
+    router.HandleFunc("/todos", TodoIndex)
+    router.HandleFunc("/todos/{todoId}", TodoShow)
+
     log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "TodoIndex!")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r) // Pega os paramêtros enviados na requisição
+    todoId := params["todoId"]
+    fmt.Fprintln(w, "TodoShow: ", todoId)
 }
