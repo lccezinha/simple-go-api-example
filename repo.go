b990b9bdc3986b4cb0eaaf310a4476083107258f
Luiz Cezer <lccezinha@gmail.com>
2015-12-25 22:17:24 -0200

    Criando um repositório fake

---
+++ repo.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+var currentId int
+var todos Todo
+
+func init() {
+    RepoCreateTodo(Todo{Name: "Estudar Golang"})
+    RepoCreateTodo(Todo{Name: "Zerar Dead Space"})
+}
+
+func generateId() int {
+    currentId += 1
+}
+
+func RepoFindTodoById(id int) Todo {
+    for _, t := todos {
+        if t.Id == id {
+            return t
+        }
+    }
+
+    return Todo{}
+}
+
+func RepoCreateTodo(todo Todo) Todo {
+    todo.Id = generateId()
+    todos = append(todos, todo)
+
+    return todo
+}
+
+func RepoDestroyTodo(id int) error {
+    for i, t := range todos {
+        if t.Id == id {
+            todos = append(todos[:i], todos[i+1:]...)
+            return nil
+        }
+    }
+
+    return fmt.Errorf("Não encontrou o ID %d do Todo para remover", id)
+}
